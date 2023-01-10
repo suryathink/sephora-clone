9c29880dfba02cb7096585cfdc281aef7e382385
Arshadiqubal17 <117514123+Arshadiqubal17@users.noreply.github.com>
2023-01-10 22:26:29 +0530

    Add files via upload

---
+++ wishlist/immport.js
@@ -0,0 +1,18 @@
+// let blank =() => {
+//     return `
+//     <div id="default" >
+//        <img src="https://www.sephora.com/img/ufe/loveless.svg" alt="">
+  
+  
+     
+//   <p ><b> You haven’t added any product to your Loves list.</b></p>
+  
+//   <p>Collect all your favorite and must-try products by clicking <br> on the ♡ while you shop.</p>
+  
+// </div>
+    
+    
+//     `
+// }
+
+// export  {blank};
